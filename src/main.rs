14ed5f8fa42bc056f8bb73ba64766a2e298bd01e
jeffrade <jrade@cars.com>
2020-02-26 17:41:43 -0600

    Adding avro-rs example with read from std::fs::File modification

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+
+use avro_rs::{Codec, Reader, Schema, Writer, from_value, types::Record};
+use failure::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct SchemaTest {
+    a: i64,
+    b: String,
+}
+
+// Contents of avro-rs-example.avsc below:
+// {
+//   "type": "record",
+//   "name": "test",
+//   "fields": [
+//     {"name": "a", "type": "long", "default": 42},
+//     {"name": "b", "type": "string"}
+//   ]
+// }
+fn main() -> Result<(), Error> {
+    let mut schema_file = File::open("avro-rs-example.avsc")?;
+    let mut raw_schema = String::new();
+    schema_file.read_to_string(&mut raw_schema)?;
+
+    let schema = Schema::parse_str(&raw_schema)?;
+
+    println!("{:?}", schema);
+
+    let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Deflate);
+
+    let mut record = Record::new(writer.schema()).unwrap();
+    record.put("a", 27i64);
+    record.put("b", "foo");
+
+    writer.append(record)?;
+
+    let test_message = SchemaTest {
+        a: 27,
+        b: "foo".to_owned(),
+    };
+
+    writer.append_ser(test_message)?;
+
+    writer.flush()?;
+
+    let input = writer.into_inner();
+    let reader = Reader::with_schema(&schema, &input[..])?;
+
+    for record in reader {
+        println!("{:?}", from_value::<SchemaTest>(&record?));
+    }
+    Ok(())
 }
